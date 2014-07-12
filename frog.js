2bd1d26237743e036c87af2c4f9de86e824b789b
Steve Barnett <steve@naga.co.za>
2014-07-12 15:06:50 +0200

    Add arrow key controls

--- frog.js
+++ frog.js
@@ -1,25 +1,25 @@
-  
+
   var lanes=[];
   var avatar;
   var board;
   var status=document.getElementById("status");
-  
-  function GameLoop(timeStamp) 
-  {        
+
+  function GameLoop(timeStamp)
+  {
 		for (var i=0; i<lanes.length;i++)
 		  positionLane(lanes[i],timeStamp)
 		moveAvatar(timeStamp)
-		window.requestAnimationFrame(GameLoop);  		
+		window.requestAnimationFrame(GameLoop);
   }
-  
+
   function resolveLaneHitTest(lane,xleft,xright)
   {
      //select my lane
      var l=lanes[lane];
 	 //map into the repeat coordinate system corrected by the lane offset
-	 xleft=(xleft-l.laneoffset)%l.repeat;	 
-	 xright=(xright-l.laneoffset)%l.repeat;	 
-	 
+	 xleft=(xleft-l.laneoffset)%l.repeat;
+	 xright=(xright-l.laneoffset)%l.repeat;
+
      //correct for current leftness of lane
      for (var i=0;i<l.members.length;i+=1) {
 	   if (xleft<xright) { //the normal order of things
@@ -27,51 +27,55 @@
 	     if (xright<l.members[i].left) continue; //our right edge is left of its left edge
 	   } else { //actually the mapping has split the points because we are overlapping the repeat
          if ((xleft>l.members[i].right)&&(xright<l.members[i].left)) continue;
-         if ((xright<l.members[i].left)&&(xleft>l.members[i].right)) continue;	     
+         if ((xright<l.members[i].left)&&(xleft>l.members[i].right)) continue;
 	   }
 	   //we ain't clear at all we got a hit!
 	   return l.members[i].type;
 	 }
 	 return "-";
   }
-  
+
   function positionLane(l,timeStamp)
   {
       l.laneoffset=(timeStamp*l.speed/1000)%l.repeat-l.repeat;
-	  l.lanecontent.style.left=l.laneoffset.toString()+"px";	  
+	  l.lanecontent.style.left=l.laneoffset.toString()+"px";
   }
-  
+
   var lastMouseX,lastMouseY;
   function touchEvent(x,y,down)
   {
     if (down) {
 	  lastMouseX=x;
-	  lastMouseY=y;	 
-      avatar.command=0;	  
+	  lastMouseY=y;
+      avatar.command=0;
 	}else {
 	  //seems we are moving...
 	  var xdif=lastMouseX-x;
 	  var ydif=lastMouseY-y;
 	  if (Math.abs(xdif)>Math.abs(ydif)) { //move sideways
 	     if (xdif>0) avatar.command=4;
-		 else avatar.command=2;		 
+		 else avatar.command=2;
 	  } else {  //move across
 	    if (ydif>0) avatar.command=1;
-		else avatar.command=3;		 
-	  }	  
+		else avatar.command=3;
+	  }
 	}
   }
-  
+
   function keyEvent(code,down)
   {
-     if (down==1) {
-	   if (code==87) avatar.command=1;
-	   if (code==68) avatar.command=2;
-	   if (code==83) avatar.command=3;
-	   if (code==65) avatar.command=4;	   
+    if (down==1) {
+    if (code==87) avatar.command=1;
+    if (code==68) avatar.command=2;
+    if (code==83) avatar.command=3;
+    if (code==65) avatar.command=4;
+    if (code==38) avatar.command=1;
+    if (code==39) avatar.command=2;
+    if (code==40) avatar.command=3;
+    if (code==37) avatar.command=4;
 	 }
   }
-  
+
   function setupBoard()
   {
      board=document.getElementsByClassName("gameframe")[0];
@@ -93,45 +97,45 @@
 		   evt.preventDefault();
 		 },false
 	     );
-		 
+
 	  board.info=document.getElementsByClassName("infopanel")[0];
 	  board.infostatus=document.getElementById("info_status");
 	  board.infocommand=document.getElementById("info_command");
 	  board.infocommand.onclick=clickBoardStatus;
-	  
+
 	  board.infogoals=document.getElementById("info_goals");
-	  board.infolives=document.getElementById("info_lives");	  
-	  
+	  board.infolives=document.getElementById("info_lives");
+
 	  board.isplaying=false;
 	  board.winState=0;
   }
-  
+
   function resetBoard()
   {
       board.focus();
 	  board.lives=5;
-	  board.goals=0;      	 
+	  board.goals=0;
   }
-  
+
   function clickBoardStatus()
   {
-      
+
 	  resetAvatar();
 	  resetBoard();
 	  board.isplaying=true;
-	  refreshBoardInfo();	 
+	  refreshBoardInfo();
   }
-  
+
   function refreshBoardInfo()
   {
       board.info.classList.toggle('small',board.isplaying);
       board.infolives.innerHTML=board.lives.toString()+" / 5 lives";
 	  board.infogoals.innerHTML=board.goals.toString()+" / 5 goals";
-	  if (board.isplaying) 
+	  if (board.isplaying)
 	    board.infocommand.innerHTML="Pause";
 	  else
 	    board.infocommand.innerHTML="Play Now";
-		
+
 	  switch (board.winState) {
 	    case 0: board.infostatus.innerHTML="Welcome"; break;
 		case 1: board.infostatus.innerHTML="Game Paused"; break;
@@ -139,33 +143,33 @@
 		case 3: board.infostatus.innerHTML="You Died"; break;
 	  }
   }
-  
+
   function notifyBoardDeath()
   {
     board.lives-=1;
 	if (board.lives==0) {
 	   board.isplaying=false;
-	   board.winState=3;	 
+	   board.winState=3;
 	}
 	refreshBoardInfo();
 	resetAvatar();
   }
-	
-	
+
+
   function notifyBoardWin()
   {
     board.goals+=1;
 	if (board.goals==5) {
 	   board.isplaying=false;
-	   board.winState=2;	 
+	   board.winState=2;
 	}
 	refreshBoardInfo();
 	resetAvatar();
   }
-	
+
   function play() {
-      setupBoard() 
-	
+      setupBoard()
+
 	  //set up lanes
       var bits=document.getElementsByClassName("lane");
       for(var i = 0; i < bits.length; i++) {
@@ -176,7 +180,7 @@
 		//add up your childrens width
 		var children=l.lanecontent.getElementsByTagName("span");
 		l.members=[];
-		for(var j = 0; j < children.length; j++)  {		   
+		for(var j = 0; j < children.length; j++)  {
 		   var ch=children[j];
 		   l.repeat+=ch.offsetWidth;
 		   if (ch.classList.contains("refuse")) {
@@ -190,25 +194,24 @@
            if (ch.classList.contains("target")) {
 		      var ob={left: ch.offsetLeft, right: ch.offsetLeft+ch.offsetWidth, type: "t"};
 			  l.members.push(ob);
-		   }		   
+		   }
 		}
 		if (l.repeat==0) l.repeat=3000;
 		//now double up on the content
-        var content=l.lanecontent.innerHTML;		
+        var content=l.lanecontent.innerHTML;
 		var start=content+content;
 		for (var e=0;e < 2000/l.repeat;e+=1)
 		  start+=content;
 		l.lanecontent.innerHTML=start;
 		l.bottomedge=l.offsetTop+l.offsetHeight;
 		lanes.push(l);
-	  }	  	  
-	  setupAvatar();	
+	  }
+	  setupAvatar();
 	  resetBoard();
 	  resetAvatar();
-	  refreshBoardInfo();	  
-	  window.requestAnimationFrame(GameLoop);     
+	  refreshBoardInfo();
+	  window.requestAnimationFrame(GameLoop);
   }
-  
+
   play();
-  
-  
+
