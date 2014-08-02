b3e8fb170b3a35b0e11ab8a5cdebbbfb0c91f4f1
Steve Barnett <steve@naga.co.za>
2014-08-02 10:20:55 +0200

    Add browserSync for local device testing

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    browserSync: {
+      bsFiles: {
+          src : 'assets/css/*.css'
+      },
+      options: {
+          server: {
+              baseDir: "./"
+          }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-browser-sync');
+
+  // Default task(s).
+  grunt.registerTask('default', ['browserSync']);
+
+};
